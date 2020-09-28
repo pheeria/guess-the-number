01f9d77147c59f3ced3947ed30d0a25298c58886
pheeria <olzhas.askar@deliveryhero.com>
2020-09-28 16:29:40 +0200

    Use Java Configuration instead of XML

---
+++ core/src/main/java/com/deliveryhero/AppConfig.java
@@ -0,0 +1,9 @@
+package com.deliveryhero;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.deliveryhero")
+public class AppConfig {
+}
--- core/src/main/java/com/deliveryhero/Main.java
+++ core/src/main/java/com/deliveryhero/Main.java
@@ -2,7 +2,7 @@ package com.deliveryhero;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
     private static final Logger log = LoggerFactory.getLogger(Main.class);
@@ -10,7 +10,7 @@ public class Main {
     public static void main(String[] args) {
         log.info("Guess The Number Game");
 
-        var context = new ClassPathXmlApplicationContext("beans.xml");
+        var context = new AnnotationConfigApplicationContext(AppConfig.class);
         var generator = context.getBean(INumberGenerator.class);
 
         log.info("Number {}",generator.next());
