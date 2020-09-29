49923d4427979cf8317482457e8c6ca25d690480
pheeria <olzhas.askar@deliveryhero.com>
2020-09-29 10:53:51 +0200

    Game works!

--- console/src/main/java/com/deliveryhero/console/Main.java
+++ console/src/main/java/com/deliveryhero/console/Main.java
@@ -1,9 +1,6 @@
 package com.deliveryhero.console;
 
 import com.deliveryhero.AppConfig;
-import com.deliveryhero.IGame;
-import com.deliveryhero.IMessageGenerator;
-import com.deliveryhero.INumberGenerator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -15,14 +12,6 @@ public class Main {
         log.info("Guess The Number Game");
 
         var context = new AnnotationConfigApplicationContext(AppConfig.class);
-        var numberGenerator = context.getBean(INumberGenerator.class);
-
-        log.info("Number {}",numberGenerator.next());
-
-        var game = context.getBean(IGame.class);
-        var messageGenerator = context.getBean(IMessageGenerator.class);
-        log.info(messageGenerator.getMainMessage());
-        log.info(messageGenerator.getResultMessage());
 
         context.close();
     }
--- console/src/main/java/com/deliveryhero/console/NumberGuess.java
+++ console/src/main/java/com/deliveryhero/console/NumberGuess.java
@@ -1,17 +1,53 @@
 package com.deliveryhero.console;
 
+import com.deliveryhero.IGame;
+import com.deliveryhero.IMessageGenerator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
+import java.util.Scanner;
+
 @Component
 public class NumberGuess {
     private static final Logger log = LoggerFactory.getLogger(NumberGuess.class);
+    @Autowired
+    private IGame game;
+    @Autowired
+    private IMessageGenerator messageGenerator;
 
     @EventListener(ContextRefreshedEvent.class)
     public void onStart() {
         log.info("Everything is ready.");
+
+        var scanner = new Scanner(System.in);
+
+        while (true) {
+
+            System.out.println(messageGenerator.getMainMessage());
+            System.out.println(messageGenerator.getResultMessage());
+
+            var guess = scanner.nextInt();
+            scanner.nextLine();
+
+            game.setGuess(guess);
+            game.check();
+
+            if (game.isGameWon() || game.isGameLost()) {
+                System.out.println(messageGenerator.getResultMessage());
+                System.out.println("Play again y/n?");
+                var answer = scanner.nextLine().trim();
+
+                if (answer.equalsIgnoreCase("n")) {
+                    break;
+                } else {
+                    game.reset();
+                }
+
+            }
+        }
     }
 }
