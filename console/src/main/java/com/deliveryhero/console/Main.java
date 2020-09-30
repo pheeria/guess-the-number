6eb7c0f20e24578d43f9faa86aa982dcb03b1195
pheeria <olzhas.askar@deliveryhero.com>
2020-09-30 10:23:15 +0200

    Setup Lombok

--- console/src/main/java/com/deliveryhero/console/Main.java
+++ console/src/main/java/com/deliveryhero/console/Main.java
@@ -1,13 +1,11 @@
 package com.deliveryhero.console;
 
 import com.deliveryhero.config.GameConfig;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+@Slf4j
 public class Main {
-    private static final Logger log = LoggerFactory.getLogger(Main.class);
-
     public static void main(String[] args) {
         log.info("Guess The Number Game");
 
